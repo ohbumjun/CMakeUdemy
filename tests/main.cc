391686938027f36ac2b83e836ab0989ba760fe0b
ohbumjun <beomjun0638@hanmail.net>
2024-01-14 18:54:07 +0900

    add comment to test

--- tests/main.cc
+++ tests/main.cc
@@ -13,5 +13,4 @@ TEST_CASE("Factorials are computed", "[factorial]")
     REQUIRE(factorial(1) == 1);
     REQUIRE(factorial(2) == 2);
     REQUIRE(factorial(3) == 6);
-    REQUIRE(factorial(10) == 3628800);
 }
